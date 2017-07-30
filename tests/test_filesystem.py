363c4f99c11fb22b5ecf70e0aea62c248a39ac08
potfur <wachowski.michal@gmail.com>
2017-07-30 17:54:43 +0200

    filesystem layer

---
+++ tests/test_filesystem.py
@@ -0,0 +1,24 @@
+from src.filesystem import FileSystem
+
+
+class TestFileSystem:
+    def setup_method(self):
+        self.fs = FileSystem()
+
+    def test_find_scans_directory(self):
+        result = self.fs.find('src/')
+        result = {repr(ref) for ref in result}
+
+        assert result == {
+            'src/__init__.py',
+            'src/filesystem.py',
+            'src/reflection.py'
+        }
+
+    def test_find_can_filter_by_file_name(self):
+        result = self.fs.find('src/', 'filesystem.py')
+        result = {repr(ref) for ref in result}
+
+        assert result == {
+            'src/filesystem.py',
+        }
