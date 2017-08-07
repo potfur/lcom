a9693977959809b0dc9694d835d2fec59d6293d6
potfur <wachowski.michal@gmail.com>
2017-08-07 22:04:55 +0200

    reflection.name returns module.class::method

--- tests/test_reflection.py
+++ tests/test_reflection.py
@@ -12,14 +12,21 @@ class ReflectionTestCase(object):
 class TestModuleReflection(ReflectionTestCase):
     def test_lists_classes(self):
         result = {cls.name() for cls in self.module.classes()}
-        assert result == {'Zero', 'One', 'Two', 'Three', 'Reflection'}
+        assert result == {
+            'tests.fixtures.Zero',
+            'tests.fixtures.One',
+            'tests.fixtures.Two',
+            'tests.fixtures.Three',
+            'tests.fixtures.Reflection'
+        }
 
 
 class TestClassReflection(ReflectionTestCase):
-    def test_list_variables(self):
-        ref = self.module.class_by_name('Reflection')
+    def setup_method(self):
+        self.ref = self.module.class_by_name('tests.fixtures.Reflection')
 
-        result = {var for var in ref.vars()}
+    def test_list_variables(self):
+        result = {var for var in self.ref.vars()}
         assert result == {
             'CONST',
             '__x',
@@ -27,60 +34,56 @@ class TestClassReflection(ReflectionTestCase):
         }
 
     def test_list_class_methods(self):
-        ref = self.module.class_by_name('Reflection')
-
-        result = {method.name() for method in ref.methods()}
+        result = {method.name() for method in self.ref.methods()}
         assert result == {
-            '__init__',
-            'get_x',
-            'get_y',
-            'methods',
-            'vars',
-            'consts',
+            'tests.fixtures.Reflection::__init__',
+            'tests.fixtures.Reflection::get_x',
+            'tests.fixtures.Reflection::get_y',
+            'tests.fixtures.Reflection::methods',
+            'tests.fixtures.Reflection::vars',
+            'tests.fixtures.Reflection::consts',
         }
 
     def test_method_by_name(self):
-        ref = self.module.class_by_name('Reflection')
-
-        assert ref.method_by_name('get_x').name() == 'get_x'
+        expected = 'tests.fixtures.Reflection::get_x'
+        assert self.ref.method_by_name('get_x').name() == expected
 
     def test_unknown_method_by_name(self):
-        ref = self.module.class_by_name('Reflection')
-
         with raises(ReflectionError) as e:
-            ref.method_by_name('foobar')
+            self.ref.method_by_name('foobar')
 
         msg = 'Unknown method %s'
         assert str(e.value) == msg % 'foobar'
 
 
 class TestMethodReflection(ReflectionTestCase):
-    def test_list_used_methods(self):
-        ref = self.module.class_by_name('Reflection') \
-            .method_by_name('methods')
+    def setup_method(self):
+        self.ref = self.module.class_by_name('tests.fixtures.Reflection')
 
-        assert set(ref.calls()) == {'get_x', 'get_y'}
+    def test_list_used_methods(self):
+        ref = self.ref.method_by_name('methods')
+
+        assert set(ref.calls()) == {
+            'tests.fixtures.Reflection::get_x',
+            'tests.fixtures.Reflection::get_y'
+        }
 
     def test_list_used_class_variables(self):
-        ref = self.module.class_by_name('Reflection') \
-            .method_by_name('consts')
+        ref = self.ref.method_by_name('consts')
 
         assert set(ref.vars()) == {'CONST'}
 
     def test_list_used_instance_variables(self):
-        ref = self.module.class_by_name('Reflection') \
-            .method_by_name('vars')
+        ref = self.ref.method_by_name('vars')
 
         assert set(ref.vars()) == {'__x', '__y'}
 
     def test_is_a_constructor(self):
-        ref = self.module.class_by_name('Reflection') \
-            .method_by_name('__init__')
+        ref = self.ref.method_by_name('__init__')
 
         assert ref.is_constructor() is True
 
     def test_is_not_a_constructor(self):
-        ref = self.module.class_by_name('Reflection') \
-            .method_by_name('methods')
+        ref = self.ref.method_by_name('methods')
 
         assert ref.is_constructor() is False
